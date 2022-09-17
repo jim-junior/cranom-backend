44aa8fce42950714c4bdf31043bbd9c8d4fb873d
Jim Junior <jimjunior854@gmail.com>
2022-09-17 15:47:58 +0300

    Asgi

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 
 #import deployments.routing
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-import os
-
-from django.core.asgi import get_asgi_application
-from .channelsmiddleware import TokenAuthMiddleware
-
-import deployments
-
-
 from deployments.routing import websocket_urlpatterns
+import deployments
+from .channelsmiddleware import TokenAuthMiddleware
+import os
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 

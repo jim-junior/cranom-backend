cbcb573ba4481de5ef58aed52c25544b73ccc697
Jim Junior <jimjunior854@gmail.com>
2022-09-17 16:03:15 +0300

    Remove auto save

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,17 +9,20 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 
 #import deployments.routing
-from .channelsmiddleware import TokenAuthMiddleware
-import deployments
-from deployments.routing import websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django_asgi_app = get_asgi_application()
+
+
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from deployments.routing import websocket_urlpatterns
+import deployments
+from .channelsmiddleware import TokenAuthMiddleware
 
 
 application = ProtocolTypeRouter({

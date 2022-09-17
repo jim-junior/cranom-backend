c7e8e0baf51b4bda2252cc628211c7995964df6e
Jim Junior <jimjunior854@gmail.com>
2022-09-17 15:55:57 +0300

    Add ksksk

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,23 +9,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 
 #import deployments.routing
-from deployments.routing import websocket_urlpatterns
-import deployments
 from .channelsmiddleware import TokenAuthMiddleware
-import os
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.routing import ProtocolTypeRouter, URLRouter
+import deployments
+from deployments.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import os
 from django.core.asgi import get_asgi_application
 
-django_asgi_app = get_asgi_application()
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 

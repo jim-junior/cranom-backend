92baece54286410ab1bf2d29d6fea4a6b37fcac4
Jim Junior <jimjunior854@gmail.com>
2023-08-26 16:42:10 +0300

    Add deploy task

--- deployments/tasks/image.py
+++ deployments/tasks/image.py
@@ -1,36 +1,55 @@
 from kubernetes import client, config, watch
 from kube.config import get_api_client_config
+from deployments.models import Node
+from deployments.utils.kube_utils.git_deployment import create_git_node_deployment, create_git_node_service, create_node_ingress
+from celery import shared_task
 
 
-apiConfig = get_api_client_config()
+""" apiConfig = get_api_client_config()
 apiclient = client.ApiClient(apiConfig)
 v1 = client.CoreV1Api(apiclient)
 apps_v1_api = client.AppsV1Api(apiclient)
 networking_v1_api = client.NetworkingV1Api(apiclient)
 
-crd_api = client.CustomObjectsApi(apiConfig)
+crd_api = client.CustomObjectsApi(apiConfig) """
+
+config.load_kube_config()
+v1 = client.CoreV1Api()
+apps_v1_api = client.AppsV1Api()
+networking_v1_api = client.NetworkingV1Api()
+crd_api = client.CustomObjectsApi()
 
 
-def watch_kp_image(image):
+@shared_task()
+def deploy_kp_image(nodeid):
+    node = Node.objects.get(id=nodeid)
+    project = node.project
+    user = project.user
+
     current = None
 
     while True:
         obj = crd_api.get_namespaced_custom_object(
             group="kpack.io",
             version="v1alpha2",
-            namespace="default",
+            namespace=user.username,
             plural="images",
-            name="image"
+            name=f"{project.name}-{node.id}-kp-image"
         )
+        # Get Kpack image build status
         status = obj["status"]["conditions"][0]["status"]
         if status == "True":
-            print("Build Passed")
+            # Deploy to kubernetes
+            create_git_node_deployment(node)
+            create_git_node_service(node)
+            create_node_ingress(node)
             break
         elif status == "False":
-            print("Build Failed")
+            node.build_status = "Failed"
+            node.save()
             break
         else:
             if current is None:
-                print("Building")
                 current = status
-                print(current)
+                node.build_status = "Building"
+                node.save()
--- deployments/utils/deployment.py
+++ deployments/utils/deployment.py
@@ -1,7 +1,7 @@
 from typing import List
 from deployments.models import Node, Project, Deployment
 import json
-from deployments.models import Deployment, Project
+from deployments.tasks.image import deploy_kp_image
 from deployments.utils.kube_utils.git_deployment import create_git_node_deployment, create_git_node_service, create_node_ingress
 from celery import shared_task
 from intergrations.models import GithubInstallation
@@ -63,46 +63,19 @@ def deploy_git_node(node: Node):
     elif node.custom_git_repo == True:
         git_username = node.git_repo_username
         git_password = node.git_repo_pswd
-    
+
     if node.is_public_repo == True:
         create_proj_sva(node, user.username)
         # create image
         create_kp_image(project, node, user.username)
-    else: 
+    else:
         create_git_secret(git_password, git_username, node.id, user.username)
         # create service account
         create_proj_sva(node, user.username)
         # create image
         create_kp_image(project, node, user.username)
 
-
-    current = None
-
-    while True:
-        obj = crd_api.get_namespaced_custom_object(
-            group="kpack.io",
-            version="v1alpha2",
-            namespace=user.username,
-            plural="images",
-            name=f"{project.name}-{node.id}-kp-image"
-        )
-        # Get Kpack image build status
-        status = obj["status"]["conditions"][0]["status"]
-        if status == "True":
-            # Deploy to kubernetes
-            create_git_node_deployment(node)
-            create_git_node_service(node)
-            create_node_ingress(node)
-            break
-        elif status == "False":
-            node.build_status = "Failed"
-            node.save()
-            break
-        else:
-            if current is None:
-                current = status
-                node.build_status = "Building"
-                node.save()
+    deploy_kp_image.delay(node.id)
 
 
 def delete_git_node(node: Node):

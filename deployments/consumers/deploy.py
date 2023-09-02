ecbab644ce6c8685e78a26b43b6228501e734891
Beingana Jim Junior <69729988+jim-junior@users.noreply.github.com>
2023-09-02 16:33:28 +0000

    Add deplos

--- deployments/consumers/deploy.py
+++ deployments/consumers/deploy.py
@@ -7,6 +7,36 @@ from channels.db import database_sync_to_async
 from deployments.models import Project, Node
 from deployments.utils.ws_token import decrypt
 from kube.config import get_api_client_config
+from kubernetes import client, config, watch
+
+
+async def get_node_logs(obj, node, username):
+    """
+    Get Logs for a specific node. This should be run in a Thread
+    """
+    """ apiConfig = get_api_client_config()
+    apiclient = client.ApiClient(apiConfig)
+    v1 = client.CoreV1Api(apiclient) """
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    w = watch.Watch()
+
+    imageName = f"{node['project']}-{node['id']}-kp-image"
+    podname = ""
+    pods = v1.list_namespaced_pod(username)
+    for pod in pods.items:
+        for key, value in pod.metadata.labels.items():
+            if key == "image.kpack.io/image":
+                if value == imageName:
+                    podname = pod.metadata.name
+                    break
+    
+    for e in w.stream(v1.read_namespaced_pod_log, name=podname, namespace=username, container="build" ):
+        if e != None:
+            obj.send(text_data=json.dumps({
+                'message': e,
+                "type": "logs"
+            }))
 
 
 class NodeDeploymentProgressConsumer(AsyncWebsocketConsumer):
